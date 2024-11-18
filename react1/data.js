222067b549d2d695c4ae7063c2f4b6126f5c4f51
Mukulkumar25 <mukul25164707@gmail.com>
2024-11-18 12:00:27 +0530

    sed

---
+++ react1/data.js
@@ -0,0 +1,9 @@
+// export let myname="Rahul";//Named export
+// export function hi(){
+//     return "Hi, working on ES6 modules";
+// }
+// export let calc=Math.random()*1000;
+function msg(){
+    return Math.random()*1000;
+}
+export default msg;
